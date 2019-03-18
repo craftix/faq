5df4d5070b792b9f1c1ce2b9847d3ef946ea1bb7
Adrien Navratil <adrien1975@live.fr>
2019-03-18 16:17:54 +0100

    Added main layout

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,18 @@
 <template>
   <div id="home">
+      <div id="nav">
 
+      </div>
+
+      <div id="content-container">
+          <div id="side">
+
+          </div>
+
+          <div id="content">
+
+          </div>
+      </div>
   </div>
 </template>
 
@@ -9,3 +21,29 @@ export default {
   name: 'home'
 }
 </script>
+
+<style>
+    #home {
+        display: flex;
+        flex-direction: column;
+    }
+
+    #nav {
+        height: 50px;
+        border-bottom: solid 1px black;
+    }
+
+    #content-container {
+        display: flex;
+        height: 100vh;
+    }
+
+    #side {
+        background-color: #aaa;
+        width: 300px;
+    }
+
+    #content {
+        width: 100%;
+    }
+</style>
